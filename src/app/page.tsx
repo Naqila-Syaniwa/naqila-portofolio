c03a22362abf1cd6c3d6a62ba90701a0868456f1
naqila syaniwa <nasyaaa312@gmail.com>
2026-07-17 23:44:21 +0700

    feat: design tokens warna, spacing, radius

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,6 +34,9 @@ export default function Home() {
             center.
           </p>
         </div>
+        <div className="bg-surface text-text-primary border-border rounded-card border p-6">
+          Tes token warna & radius
+        </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
             className="bg-foreground text-background flex h-12 w-full items-center justify-center gap-2 rounded-full px-5 transition-colors hover:bg-[#383838] md:w-[158px] dark:hover:bg-[#ccc]"
