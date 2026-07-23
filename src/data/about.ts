c12fb4906b6fae29f00e1c769d4a802afd29b65e
naqila syaniwa <nasyaaa312@gmail.com>
2026-07-23 19:10:44 +0700

    feat: add inital About Me page and content data

---
+++ src/data/about.ts
@@ -0,0 +1,10 @@
+export const aboutContent = {
+    name: 'Naqila Syaniwa',
+    initials: 'NS',
+    roles: ['Software Engineer', 'UI/UX Designer', 'Game Developer', 'Full Stack Engineer'],
+    bio: [
+        'Description bio about myself, what im working, and what i like',
+        'Second paragraph is about my passion or interent like projects'
+    ],
+    cvUrl: '#',
+};
